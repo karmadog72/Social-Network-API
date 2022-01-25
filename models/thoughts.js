e7444b51d0055076bb1597d808d8feac021c8313
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2022-01-25 15:24:33 -0700

    added user thoughts schema

---
+++ models/thoughts.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+const thoughtsSchema = new Schema(
+  {
+    thoughtsText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionsSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtsSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thoughts = model("Thoughts", ThoughtsSchema);
+
+module.exports = Thoughts;
